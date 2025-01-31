040ddaaf1ee1626793fa9a8b87f347fa3070b0a6
PolinaBasargina <polbas05@mail.ru>
2025-01-31 18:48:20 +0700

    feat: add main page and api requests

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 import Header from './Header.js'
 import Footer from './Footer.js'
+import Form_departure_city from './Form_departure_city.js'
+import Form_destination_city from './Form_destination_city.js'
 
 new Header ()
 new Footer ()
+new Form_departure_city ()
+new Form_destination_city ()
