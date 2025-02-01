d1580c66d0191494206f9a5ee774e1b17307059d
PolinaBasargina <polbas05@mail.ru>
2025-02-01 13:26:05 +0700

    feat: add box size selection

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import Header from './Header.js'
 import Footer from './Footer.js'
 import Form_departure_city from './Form_departure_city.js'
 import Form_destination_city from './Form_destination_city.js'
+import Form_package_size from './Form_package_size.js'
 
 new Header ()
 new Footer ()
 new Form_departure_city ()
 new Form_destination_city ()
+new Form_package_size ()
